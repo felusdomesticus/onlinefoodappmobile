bddadef049efd7b7ae3057821e3764c2a4382937
mesutcan <mesutcan0632@hotmail.com>
2019-10-24 22:46:48 +0300

    - Sliding Meals

--- app/src/main/java/com/bilgeadam/mobilefoodapp/MainActivity.java
+++ app/src/main/java/com/bilgeadam/mobilefoodapp/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main3);
         authenticate();
         configureSlider();
     }
